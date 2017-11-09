1f287756f796bba11386041c683e347a1375056c
Guillermo Bego <guillermo.beltran@schibsted.com.mx>
2017-11-09 10:27:01 -0600

    Add html-webpack-plugin and css loaders

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,15 +1,44 @@
 const commonPaths = require("./common-paths");
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 const config = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "[chunkhash].bundle.js",
     path: commonPaths.outputPath
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Rocking app',
+      template: './src/index.html',
+      inject: 'body',
+      favicon: './src/assets/img/favicon.ico'
+    })
+  ],
   module: {
+    rules: [
+      {
+        test: /[\.png|\.ico]$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 100000
+          }
+        }]
+      }, {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }]
+      }
+    ],
     loaders: [{
       test: /\.js$/,
-      exclude: /node_modules/,
+      exclude: /node_modules\//,
       loader: 'babel-loader'
     }]
   }
