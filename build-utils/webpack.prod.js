ce13a53c4f2acc9467f42a8bb812cfbf5d033f66
Guillermo Bego <guillermo.beltran@schibsted.com.mx>
2017-11-09 10:28:14 -0600

    Add babel loader for prod config

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,7 +1,15 @@
 const webpack = require("webpack")
 
 module.exports = {
+  devtool: "source-map",
   plugins: [
     new webpack.optimize.UglifyJsPlugin()
-  ]
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: [{ loader: 'babel-loader', options: { cacheDirectory: true } }],
+      exclude: /node_modules(?!\/webpack-dev-server)/,
+    }]
+  }
 }
