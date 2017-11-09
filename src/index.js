bdb1de21eea5a8a38bb8c4471fd5303674d80b4c
Guillermo Bego <guillermo.beltran@schibsted.com.mx>
2017-11-09 10:29:37 -0600

    Add css and content to visualize in browser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './assets/css/styles.css';
+
 const greet = (name) => {
   return `Hola ${name}!`;
 }
-
-const greeting = greet("Bego");
-console.log(greeting);
+const salut = document.createElement("div");
+salut.innerText = greet("Bego");
+const body = document.querySelector("#root");
+body.appendChild(salut);
